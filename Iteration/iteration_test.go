5e96ea59109deb1d4748db21d9d0886f5737c2a0
Berkeli Halmyradov <berkelih@gmail.com>
2022-08-25 19:48:49 +0100

    Iterations

---
+++ Iteration/iteration_test.go
@@ -0,0 +1,40 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	t.Run("should repeat 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("should repeat 8 times", func(t *testing.T) {
+		repeated := Repeat("ba", 8)
+		expected := "babababababababa"
+
+		assertCorrectMessage(t, repeated, expected)
+	})
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Outputs "aaaaa"
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func assertCorrectMessage(t *testing.T, repeated, expected string) {
+	if repeated != expected {
+		t.Errorf("got %s want %s", repeated, expected)
+	}
+}
